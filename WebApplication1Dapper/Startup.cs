aba916546d9532eff83ee21376a927aa8ddda46a
Thoba Shangase <shangasett@outlook.com>
2022-09-05 17:34:08 +0200

    Upgrade to .net core 3.1 from 2.1

--- WebApplication1Dapper/Startup.cs
+++ WebApplication1Dapper/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WebApplication1Dapper.Logic;
 
 namespace WebApplication1Dapper
@@ -33,11 +34,11 @@ namespace WebApplication1Dapper
             });
 
             services.AddScoped<IPeopleLogic, PeopleLogic>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -53,11 +54,11 @@ namespace WebApplication1Dapper
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
